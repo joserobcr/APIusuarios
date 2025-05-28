6c5532858f99a5a2fcff687ff8369f4de9eb648b
joserobcr <joserobcr2003@hotmail.com>
2025-05-28 09:16:37 -0500

    tercer commit

--- index.js
+++ index.js
@@ -25,7 +25,12 @@ const logger = winston.createLogger({
 app.use(express.json({ limit: '1mb' }));
 app.use(express.text({ limit: '1mb' }));
 app.use(xmlparser());
-app.use(cors());
+app.use(cors({
+  origin: 'http://apiusuarios-production-c528.up.railway.app',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
+
 
 // Configurar Pug
 app.set('view engine', 'pug');
