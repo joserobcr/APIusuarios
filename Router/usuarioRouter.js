7aa0e1b4f022b087d944a05e5d4463580fdc8ecb
joserobcr <joserobcr2003@hotmail.com>
2025-05-28 09:39:57 -0500

    sexto commit

--- Router/usuarioRouter.js
+++ Router/usuarioRouter.js
@@ -4,13 +4,13 @@ const hal = require('hal');
 const halson = require('halson');
 const usuarioController = require('../Controller/usuarioController.js');
 
-router.get('/', (req, res) => {
+/*router.get('/', (req, res) => {
     let opciones = {
         titulo: 'Lista de Usuarios',
         mensaje: 'Aquí puedes ver la lista de usuarios'
     };
     res.render('index', opciones); // Renderiza "index.pug" dentro de "views"
-});
+});*/
 router.get('/', usuarioController.consultarUsuario);
 //router.post('/', usuarioController.agregarUsuario); 
 
